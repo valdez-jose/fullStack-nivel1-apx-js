e0398998cc911c3b916c7ee2c102639c1f3f2656
valdez-jose <jusepetony7@gmail.com>
2025-08-12 21:21:54 -0300

    modified:introduccion.js

--- estructura-de-control/bucles/introduccion.js
+++ estructura-de-control/bucles/introduccion.js
@@ -10,5 +10,14 @@ do...while → repite al menos una vez y luego verifica la condición.
 
 Bucles especiales como for...of y for...in → para recorrer colecciones o propiedades.
 */
+/*Modificá este while para que el counter se modifique en cada ejecución del while y que se ejecute solo 10 0 11 veces. Al finalizar mostrá un mensaje usando console.log() que diga "Terminó"
+*/
 
+let counter = 0;
 
+while (counter <11) {
+  console.log("Contando: " + counter);
+  counter++;
+}
+
+console.log("Terminó");
